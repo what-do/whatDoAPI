bddc94a63e26607352b7d99295e8c67388ecca89
jaimegarciae <jaime.garcia.echanove@gmail.com>
2018-04-16 21:00:07 -0500

    Creates Tag subschema, properly adds tags to activities

--- activity/Activity.js
+++ activity/Activity.js
@@ -1,12 +1,26 @@
 var mongoose = require('mongoose');  
+
+var TagSchema = new mongoose.Schema({
+  alias: String,
+  title: String
+},{ _id : false });
+
 var ActivitySchema = new mongoose.Schema({  
   name: String,
   type: String,
   image: String,
   description: String,
   yelp: String,
-  tags: [{type: String}],
-  address: [{type: String}]
+  tags: [TagSchema],
+  address: [{
+  	city: String,
+  	country: String,	
+  	address2: String,
+  	address3: String,
+    state: String,
+    address1: String,
+    zip_code: String
+  }],
 });
 mongoose.model('Activity', ActivitySchema);
 module.exports = mongoose.model('Activity');
--- activity/ActivityController.js
+++ activity/ActivityController.js
@@ -13,17 +13,29 @@ router.post("/", function (req, res) {
         type: req.body.type,
         name: req.body.name,
         image: req.body.image,
-        tags: req.body.tags,
+        tags: [],
         address: req.body.address,
         description: req.body.description,
         yelp: req.body.yelp
 		},
 		function(err, activity) {
 			if(err) return res.status(500).send("There was a problem adding the information to the database.");
+            addTags(JSON.parse(req.body.tags), activity);
+            addAddress(JSON.parse(req.body.address), activity);
             res.status(200).send(activity);
 		}
 	);
 });
+
+function addTags(parsedTags, activity){
+    for(var i = 0; i < parsedTags.length; i++) {
+        activity.tags.push(parsedTags[i]);
+    }
+}
+
+function addAddress(parsedAddress, activity){
+
+}
 
 //Returns all Activities in DB
 router.get('/', function (req, res) {
