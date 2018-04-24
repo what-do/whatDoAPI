85a04a4de603b913b0fa3c9b0ba7cae42ac61493
bdoobie01 <brian.dubb01@gmail.com>
2018-04-23 20:26:02 -0500

    Added remove prevouis function

--- activity/ActivityController.js
+++ activity/ActivityController.js
@@ -20,6 +20,14 @@ router.post("/", function (req, res) {
 		},
 		function(err, activity) {
 			if(err) return res.status(500).send("There was a problem adding the information to the database.");
+
+            Activity.remove({name: activity.name, address: activity.address}, 
+                function(err){
+                    if(err){
+                        res.status(500).send('There was an error removing a previous entry: ' + err.message);
+                    }
+                });
+
             addTags(JSON.parse(req.body.tags), activity);
             activity.address.push(JSON.parse(req.body.address));
             activity.save();
