8517b361791daa3598a1eec332c5d92433e06d5d
bdoobie01 <brian.dubb01@gmail.com>
2018-05-03 14:50:10 -0500

    same

--- YelpScripting/yelpImp1.js
+++ YelpScripting/yelpImp1.js
@@ -96,7 +96,7 @@ const options = {
   }
 };
 
-const req = http.request(options, (res) => {
+const req = https.request(options, (res) => {
   //console.log(`STATUS: ${res.statusCode}`);
   res.setEncoding('utf8');
   res.on('data', (chunk) => {
